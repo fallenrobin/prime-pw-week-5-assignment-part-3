be97bcda45ffa40323c1706d99eba2c8d78354ba
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-02-15 15:35:47 -0600

    created function addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,21 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  //console.log('inside addToCollection');//just for testing
+  //for (var variable in object) {
+    title = title;
+    artist = artist;
+    yearPublished = yearPublished;
+//}//end for in// NOPE do not need a loop here
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  }//end "album"
+  collection.push(album);
+  return album;
+}//end addToCollection
+
+addToCollection('banana', 'monkey', 2022);
+console.log(collection);
